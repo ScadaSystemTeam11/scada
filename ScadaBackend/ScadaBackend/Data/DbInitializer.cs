1d078a1b752b0525062b0333415194b007a831c1
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-08-06 18:34:26 +0200

    update database

--- ScadaBackend/ScadaBackend/Data/DbInitializer.cs
+++ ScadaBackend/ScadaBackend/Data/DbInitializer.cs
@@ -52,9 +52,7 @@ public static class DbInitializer
           new ("Kontrola rezervoara 1", "Distribucija cokoladnog mleka",
               10, 18, 3, 50, "L" ),
           new ("Kontrola rezervoara 3", "Distribucija sojinog mleka",
-              10, 30, 3, 50, "L" ),
-          new ("Kontrola rezervoara 5", "Distribucija kokosovog mleka",
-              10, 5, 3, 50, "L" )
+              10, 30, 3, 50, "L" )
         };
 
         foreach (AnalogOutput d in analogOutputs)
@@ -68,12 +66,10 @@ public static class DbInitializer
     {
         var analogInputs = new AnalogInput[]
         {
-           new ("Rezervoar 1", "Rezervoar za cokoladno mleko", 18, 4,
+           new ("Rezervoar 1", "Rezervoar za cokoladno mleko", 18, 3,
                true, new List<Alarm>(), 3, 50, "L", "Driver1"),
-           new ("Rezervoar 3", "Rezervoar za sojino mleko", 30, 4,
-               true, new List<Alarm>(), 3, 50, "L", "Driver1"),
-           new ("Rezervoar 5", "Rezervoar za kokosovo mleko", 5, 4,
-               true, new List<Alarm>(), 3, 50, "L", "Driver1"),
+           new ("Rezervoar 3", "Rezervoar za sojino mleko", 30, 3,
+               true, new List<Alarm>(), 3, 50, "L", "Driver1")
 
         };
 
@@ -91,8 +87,7 @@ public static class DbInitializer
         {
             new ("Kontrola ventila 14", "Pustanje tecnosti u rezervoar", 0, 0),
             new ("Kontrola ventila 22", "Pustanje tecnosti u rezervoar", 0, 0),
-            new ("Kontrola ventila 96", "Izlivanje tecnosti iz rezervoara", 0, 0),
-            new ("Kontrola ventila 64", "Izlivanje tecnosti iz rezervoara", 0, 0),
+          
 
         };
 
@@ -110,8 +105,7 @@ public static class DbInitializer
         {
             new ("Ventil 14", "Dovod tecnosti", 3, true, "driver1", 0),
             new ("Ventil 22", "Dovod tecnosti", 3, true, "driver1", 0),
-            new ("Ventil 96", "Odvod tecnosti", 3, true, "driver1", 0),
-            new ("Ventil 64", "Odvod tecnosti", 3, true, "driver1", 0)
+         
         };
 
         foreach (DigitalInput d in digitalInputs)
